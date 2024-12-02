a47ee15c5c499eab0a762a63619b9e9c82c4ef68
FabioCabrera <fcabreram1@upao.edu.pe>
2024-12-02 18:02:31 -0500

    update

--- backend/middlewares/error.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-        message: 'Algo salió mal en el servidor',
-        error: err.message,
-    });
-};
-
-module.exports = errorHandler;
