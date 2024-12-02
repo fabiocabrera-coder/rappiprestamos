a962c06cd1fa1148341cc12add506d6062b046ee
FabioCabrera <fcabreram1@upao.edu.pe>
2024-12-02 16:53:26 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import path from "path";
 import { fileURLToPath } from 'url';
-import 'dotenv/config'
+
 import express from 'express';  // Usar import para cargar express
 import { router as clienteRoutes } from './routes/clienteRoutes.js';  // Cambia a importar la exportación explícita
 import prestamoRoutes from './routes/prestamoRoutes.js';
@@ -8,14 +8,18 @@ import pagoRoutes from './routes/pagoRoutes.js';
 import publicRoutes from './routes/publicRoutes.js';
 import cors from 'cors';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json()); // Para parsear las solicitudes con JSON
-
+app.use(express.static(path.join(__dirname, 'frontend/html')));
 // Rutas
+app.use('/pagina-principal', publicRoutes);
 app.use('/api/clientes', clienteRoutes);  
 app.use('/api/prestamos', prestamoRoutes);
 app.use('/api/pagos', pagoRoutes);
