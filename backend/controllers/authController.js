27a4bd6d4701dffd678f9b5c5ae7c10af4e74a35
FabioCabrera <fcabreram1@upao.edu.pe>
2024-12-03 13:16:16 -0500

    config update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const Usuario = require('../models/Usuario'); // Modelo de Usuario
+const Usuario = require('../../models/Usuario'); // Modelo de Usuario
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
