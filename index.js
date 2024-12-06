0331f9ee7ad7e12f2aa9485ad158c946a5b136eb
FabioCabrera <fcabreram1@upao.edu.pe>
2024-12-05 22:56:50 -0500

    actualización index

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import path from "path";
 import { fileURLToPath } from 'url';
 
 import express from 'express';  // Usar import para cargar express
-import { router as clienteRoutes } from './routes/clienteRoutes.js';  // Cambia a importar la exportación explícita
-import prestamoRoutes from './routes/prestamoRoutes.js';
-import pagoRoutes from './routes/pagoRoutes.js';
-import publicRoutes from './routes/publicRoutes.js';
+import { router as clienteRoutes } from './backend/routes/clienteRoutes.js';  // Cambia a importar la exportación explícita
+import prestamoRoutes from './backend/routes/prestamoRoutes.js';
+import pagoRoutes from './backend/routes/pagoRoutes.js';
+import publicRoutes from './backend/routes/publicRoutes.js';
 import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -20,7 +20,7 @@ app.use(express.json()); // Para parsear las solicitudes con JSON
 app.use(express.static(path.join(__dirname, '../frontend/html')));
 // Rutas
 app.use('/pagina-principal', publicRoutes);
-app.use('/api/clientes', clienteRoutes);  
+app.use('/backend/api/clientes', clienteRoutes);  
 app.use('/api/prestamos', prestamoRoutes);
 app.use('/api/pagos', pagoRoutes);
 
